42263181cd26c1020d26ceabe8ccce0469124eff
atmosinf <73055038+atmosinf@users.noreply.github.com>
2021-11-16 14:54:28 +0200

    added notes

---
+++ Topic 8/README.md
@@ -0,0 +1,4 @@
+## Screenshots<br>
+
+### Dynamic php footer that displays date and time
+![1](screenshots/1.PNG)<br><br>
---
+++ Topic 8/index.php
@@ -0,0 +1,33 @@
+
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <title>php example</title>
+</head>
+<body>
+<div class="p-5 mb-4 bg-light rounded-3">
+      <div class="container-fluid py-5">
+        <h1 class="display-5 fw-bold">php example</h1>
+        <p class="col-md-8 fs-4">The footer dynamically shows the date and time using php</p>
+      </div>
+    </div>
+ <div class="container">
+  <footer class="py-3 my-4">
+    <ul class="nav justify-content-center border-bottom pb-3 mb-3">
+      <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">Home</a></li>
+      <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">Features</a></li>
+      <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">Pricing</a></li>
+      <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">FAQs</a></li>
+      <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">About</a></li>
+    </ul>
+    <p class="text-center text-muted">© <?php echo date("d.m.Y, h:i:s"); ?> Copyright.
+    </p>
+  </footer>
+</div>
+</body>
+</html>
+
+
